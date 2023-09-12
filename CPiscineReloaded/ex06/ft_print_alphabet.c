842bbfb6c59c72361db5554b3767580fabdabfac
Marc Descalzo Gree <mdescalz@car7s1.42barcelona.com>
2023-09-12 11:04:35 +0200

    CPiscineReloaded

---
+++ CPiscineReloaded/ex06/ft_print_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdescalz <mdescalz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 10:02:59 by mdescalz          #+#    #+#             */
+/*   Updated: 2023/09/12 11:03:00 by mdescalz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	int	i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		ft_putchar(i);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+}*/
---
+++ CPiscineReloaded/ex07/ft_print_numbers.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdescalz <mdescalz@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 10:42:50 by mdescalz          #+#    #+#             */
+/*   Updated: 2023/09/12 11:02:42 by mdescalz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		ft_putchar(i);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+/*int	main(void)
+{
+	ft_print_numbers();
+}*/
