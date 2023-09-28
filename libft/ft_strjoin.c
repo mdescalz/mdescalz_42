96b848ca46cac250541289a49abe376a3d3bfa18
Marc Descalzo Gree <mdescalz@car10s2.42barcelona.com>
2023-09-28 13:20:21 +0200

    strjoin update

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdescalz <mdescalz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 13:10:33 by mdescalz          #+#    #+#             */
+/*   Updated: 2023/09/28 13:19:55 by mdescalz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Allocates (with malloc(3)) and returns a new string,
+which is the result of the concatenation of ’s1’ and ’s2’.
+s1: The prefix string.
+s2: The suffix string.
+return values
+The new string.
+NULL if the allocation fails.*/
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *ptr;
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ptr)
+		return (NULL);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
