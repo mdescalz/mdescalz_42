082d5cc3d18cce98575bb689d8d0a525c4639a3e
mdescalz <129546045+mdescalz@users.noreply.github.com>
2023-10-11 11:51:00 +0200

    casi

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,12 +6,15 @@
 /*   By: mdescalz <mdescalz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 11:10:26 by mdescalz          #+#    #+#             */
-/*   Updated: 2023/10/11 11:40:19 by mdescalz         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:50:21 by mdescalz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
+Adds the node ’new’ at the end of the list.
+lst:  The address of a pointer to the first link of a list.
+new:  The address of a pointer to the node to be added to the list.*/
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,10 +6,14 @@
 /*   By: mdescalz <mdescalz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 11:10:31 by mdescalz          #+#    #+#             */
-/*   Updated: 2023/10/11 11:16:53 by mdescalz         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:50:46 by mdescalz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Adds the node ’new’ at the beginning of the list.
+lst:  The address of a pointer to the first link of a list.
+new:  The address of a pointer to the node to be added to the list.*/
 #include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
