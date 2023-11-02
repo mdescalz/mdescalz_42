1d628a429d0b188cf9cf24d52ed6718444ef110b
mdescalz <129546045+mdescalz@users.noreply.github.com>
2023-11-02 09:26:30 +0100

    empezar get next line

---
+++ get_next_line/get_next_lline.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_lline.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdescalz <mdescalz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 12:13:02 by mdescalz          #+#    #+#             */
+/*   Updated: 2023/11/01 12:15:12 by mdescalz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
